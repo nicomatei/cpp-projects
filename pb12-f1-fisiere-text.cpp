987704429f26bd8fdd70d6f754809e13986028e4
Nico Matei <mateinikoleta10@gmail.com>
2026-02-16 23:34:58 +0200

    pb 12 rezolvata

--- pb12-f1-fisiere-text.cpp
+++ pb12-f1-fisiere-text.cpp
@@ -1,2 +1,27 @@
 // 12. Dintre cele n numere situate in fisierul NR.TXT, afisati-le pe cele care
 // sunt echilibrate (au prima cifra egala cu ultima)
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    ifstream fin("in/16-02-26-nr.txt");
+    int n, nr;
+    fin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        fin >> nr;
+        int copNr = nr;
+        while (copNr > 9)
+        {
+            copNr = copNr / 10;
+        }
+        if (nr % 10 == copNr % 10)
+        {
+            cout << nr << " ";
+        }
+    }
+    fin.close();
+    return 0;
+}
