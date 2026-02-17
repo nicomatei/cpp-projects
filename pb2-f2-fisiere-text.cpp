d1c24ca16bcf2fe690a4d394f2312193761f1592
Nico Matei <mateinikoleta10@gmail.com>
2026-02-17 11:41:19 +0200

    cppmain shortcut

---
+++ pb2-f2-fisiere-text.cpp
@@ -0,0 +1,12 @@
+/* DATE.IN contine cel puțin 2 si cel mult 1 milion de numere intregi de maxim 9 cifre fiecare.
+Determinati cele mai mari 3 numere distincte din fisier. (variantă: aceeași cerință dar
+numerele să nu fie neapărat distincte) */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
