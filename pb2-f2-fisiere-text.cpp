be3adc137672484c8c4483f7a9b0006f51ef5d55
Nico Matei <mateinikoleta10@gmail.com>
2026-02-18 23:11:40 +0200

    rezolvare pb 2 f2

--- pb2-f2-fisiere-text.cpp
+++ pb2-f2-fisiere-text.cpp
@@ -3,10 +3,37 @@ Determinati cele mai mari 3 numere distincte din fisier. (variantă: aceeași ce
 numerele să nu fie neapărat distincte) */
 
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-
+    ifstream f("in/17-02-26-ddate.in");
+    int a, max1, max2, max3; // max1 > max2 > max3
+    max1 = INT_MIN + 2;
+    max2 = INT_MIN + 1;
+    max3 = INT_MIN;
+    while (f >> a)
+    {
+        if (a != max1 && a != max2 && a != max3)
+        {
+            if (a > max1)
+            {
+                max3 = max2;
+                max2 = max1;
+                max1 = a;
+            }
+            else if (a > max2)
+            {
+                max3 = max2;
+                max2 = a;
+            }
+            else if (a > max3)
+                max3 = a;
+        }
+    }
+    cout << max1 << " " << max2 << " " << max3 << endl;
+    f.close();
     return 0;
 }
