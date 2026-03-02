67f27b9c9913720a5482c3da8a4fb41ddd4c943c
Nico Matei <mateinikoleta10@gmail.com>
2026-03-02 10:10:15 +0200

    rez complet

---
+++ test-fisiere-text.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream fin("24-02-26-date.in");
+    ofstream fout("24-02-26-date.out");
+
+    int n, nr, ionel, gigel, ultim_i, ultim_g;
+
+    fin.close();
+    fout.close();
+    return 0;
+}
