03e8fa5c1b62b32590f97d61814fa7f14dcc104e
Nico Matei <mateinikoleta10@gmail.com>
2026-05-18 19:20:49 +0300

    miscare piese dame

--- dame-matrici.cpp
+++ dame-matrici.cpp
@@ -17,22 +17,13 @@ void printEmoji(int value)
 
 void printLetter(int value)
 {
-    if (value == 1)
-        cout << "a ";
-    if (value == 2)
-        cout << "b ";
-    if (value == 3)
-        cout << "c ";
-    if (value == 4)
-        cout << "d ";
-    if (value == 5)
-        cout << "e ";
-    if (value == 6)
-        cout << "f ";
-    if (value == 7)
-        cout << "g ";
-    if (value == 8)
-        cout << "h ";
+    char l = char(96 + value);
+    cout << l << " ";
+}
+
+int getLetterPoz(char l)
+{
+    return (l - '0') - 49;
 }
 
 void afisare(int tabla[8][8], int linii[8], int coloane[8])
@@ -81,9 +72,36 @@ int main()
     // afisare
     afisare(tabla, linii, coloane);
     cout << endl;
+
+    // string moveFrom = "d6";
+    // string moveTo = "e5";
+
+    string moveFrom, moveTo;
+    cout << "player 🔵: " << endl;
+    cout << "selecteaza piesa pe care vrei sa o muti (ex: e3): ";
+    cin >> moveFrom;
+    cout << "unde sa o muti (ex: f4): ";
+    cin >> moveTo;
+    cout << moveFrom << " -> " << moveTo << endl;
+    int x1 = getLetterPoz(moveFrom[0]);
+    int y1 = 8 - (moveFrom[1] - '0');
+    int x2 = getLetterPoz(moveTo[0]);
+    int y2 = 8 - (moveTo[1] - '0');
+    if (tabla[y2][x2] == 9)
+    {
+        swap(tabla[y1][x1], tabla[y2][x2]);
+        afisare(tabla, linii, coloane);
+    }
+
     // swap(tabla[2][3], tabla[3][4]);
     // cout << endl;
     // afisare(tabla, linii, coloane);
 
+    // int poz = getLetter('a');
+    // cout << poz;
+    // poz = getLetter('b');
+    // cout << poz;
+    // poz = getLetter('h');
+    // cout << poz;
     return 0;
 }
