fe38c5ae3faabfb976ef8f89266ff79230f743d3
Nico Matei <mateinikoleta10@gmail.com>
2026-05-18 22:49:08 +0300

    mancat de dame

--- dame-matrici.cpp
+++ dame-matrici.cpp
@@ -73,35 +73,93 @@ int main()
     afisare(tabla, linii, coloane);
     cout << endl;
 
-    // string moveFrom = "d6";
-    // string moveTo = "e5";
-
     string moveFrom, moveTo;
-    cout << "player 🔵: " << endl;
+    int playerMove = 1; // det a cui ii tura
+    cout << endl
+         << "player 🔵: " << endl;
     cout << "selecteaza piesa pe care vrei sa o muti (ex: e3): ";
     cin >> moveFrom;
     cout << "unde sa o muti (ex: f4): ";
     cin >> moveTo;
-    cout << moveFrom << " -> " << moveTo << endl;
+    cout << moveFrom << " -> " << moveTo << endl
+         << endl;
     int x1 = getLetterPoz(moveFrom[0]);
     int y1 = 8 - (moveFrom[1] - '0');
     int x2 = getLetterPoz(moveTo[0]);
     int y2 = 8 - (moveTo[1] - '0');
-    if (tabla[y2][x2] == 9)
+    if (tabla[y2][x2] == 9 && (tabla[y1][x1] == 1 && playerMove == 1))
     {
         swap(tabla[y1][x1], tabla[y2][x2]);
         afisare(tabla, linii, coloane);
+        if (playerMove == 1)
+            playerMove = 2;
+        else
+            playerMove = 1;
+    }
+    else
+        cout << "invalide move" << endl;
+    cout << endl;
+    cout << endl;
+    cout << "selecteaza piesa + spatiu + unde o muti (ex: d6 c5): ";
+
+    bool valid = true;
+    while (true)
+    {
+        valid = false;
+        for (i = 1; i < 8; i++)
+            for (j = 1; j < 8; j++)
+                if (tabla[i][j] == 1 || tabla[i][j] == 2)
+                {
+                    valid = true;
+                }
+
+        string moveFrom, moveTo;
+        if (playerMove == 1)
+            cout << "player 🔵: ";
+        else
+            cout << "player 🔴: ";
+        cin >> moveFrom >> moveTo;
+        x1 = getLetterPoz(moveFrom[0]);
+        y1 = 8 - (moveFrom[1] - '0');
+        x2 = getLetterPoz(moveTo[0]);
+        y2 = 8 - (moveTo[1] - '0');
+        if (tabla[y2][x2] == 9 && ((tabla[y1][x1] == 1 && playerMove == 1) || (tabla[y1][x1] == 2 && playerMove == 2)))
+        {
+            swap(tabla[y1][x1], tabla[y2][x2]);
+
+            // daca sare peste o dama adversara pe diagonala o manana
+            if (playerMove == 1)
+            {
+                if (tabla[y1 - 1][x1 + 1] == 2 && tabla[y2][x2] == 1)
+                {
+                    tabla[y1 - 1][x1 + 1] = 9;
+                }
+                if (tabla[y1 - 1][x1 - 1] == 2 && tabla[y2][x2] == 1)
+                {
+                    tabla[y1 - 1][x1 - 1] = 9;
+                }
+            }
+            if (playerMove == 2)
+            {
+                if (tabla[y1 + 1][x1 + 1] == 1 && tabla[y2][x2] == 2)
+                {
+                    tabla[y1 + 1][x1 + 1] = 9;
+                }
+                if (tabla[y1 + 1][x1 - 1] == 1 && tabla[y2][x2] == 2)
+                {
+                    tabla[y1 + 1][x1 - 1] = 9;
+                }
+            }
+
+            afisare(tabla, linii, coloane);
+            if (playerMove == 1)
+                playerMove = 2;
+            else
+                playerMove = 1;
+        }
+        else
+            cout << "invalide move" << endl;
     }
 
-    // swap(tabla[2][3], tabla[3][4]);
-    // cout << endl;
-    // afisare(tabla, linii, coloane);
-
-    // int poz = getLetter('a');
-    // cout << poz;
-    // poz = getLetter('b');
-    // cout << poz;
-    // poz = getLetter('h');
-    // cout << poz;
     return 0;
 }
